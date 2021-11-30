0d9c881de1e199227b173970affbd9b0dc040080
VusheshDookheea <20048553@tafe.wa.edu.au>
2021-11-30 13:22:43 +0800

    Feature User Input
    Implemented user inputs and saving changes to a file

--- main.py
+++ main.py
@@ -1,16 +1,14 @@
-# This is a sample Python script.
+from game import ChessBoard
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
-
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+chessboard = ChessBoard()
+print(chessboard)
+while True:
+    try:
+        move = str(input('Move (example : e2e4) : '))
+        chessboard.move(move)
+        file = open('Chessboard.txt','a')
+        file.write(str(chessboard)+'\n')
+        file.close()
+        print(chessboard)
+    except Exception as e:
+        print(str(e))
